2dffb0d47b66d6834519e79bd2e5bf77a5852d1a
Håkon Mardal <hakon0809@hotmail.com>
2018-10-05 00:52:16 +0200

    #6 laget basic statisk avtale komponent som viser en avtale

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import Appointment from './components/Appointment.js';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+				<Appointment 
+					header="jobbintervju"
+					date="2018-10-10"
+					description="Jobbintervju hos zedge, blir sykt fet ass">
+				</Appointment>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
