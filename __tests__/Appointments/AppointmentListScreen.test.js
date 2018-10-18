033c3abb824176d7ef71389a01f9f306c8eb78b6
Håkon Mardal <hakon0809@hotmail.com>
2018-10-19 00:55:44 +0200

    #18 fikset en enkel test som sjekker at AppointmentList renderer riktig

--- __tests__/Appointments/AppointmentListScreen.test.js
+++ __tests__/Appointments/AppointmentListScreen.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import CreateAppointment from '../../components/Appointments/CreateAppointmentScreen.js';
+import AppointmentListScreen from '../../components/Appointments/AppointmentListScreen.js';
 import renderer from 'react-test-renderer';
 import ShallowRenderer from 'react-test-renderer/shallow';
 
 it("renders correctly", () => {
-    const tree = renderer.create(<CreateAppointment />).toJSON();
-    expect(tree).toMatchSnapshot() ;
+    const tree = renderer.create(<AppointmentListScreen />).toJSON();
+    expect(tree).toMatchSnapshot();
 })
