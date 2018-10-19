e6d883baa09bdef019e983e1cdbca7295704abd7
lefso <leif.ulvund@gmail.com>
2018-10-19 13:52:04 +0200

    Oppdateringer til package

--- __tests__/App/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import HomeScreen from '../../App.js';
-import renderer from 'react-test-renderer';
-import ShallowRenderer from 'react-test-renderer/shallow';
-
-it("renders correctly", () => {
-    const tree = renderer.create(<HomeScreen />).toJSON();
-    expect(tree).toMatchSnapshot();
-})
